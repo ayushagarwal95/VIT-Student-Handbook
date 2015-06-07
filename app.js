a77675e6afffbe80f9a6924bffb0159986958ac9
ayushagarwal95 <agarwalayush161@gmail.com>
2015-06-07 20:22:34 +0530

    Reformatted Code

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var mongoURI = process.env.MONGO_LAB_URI || 'mongodb://localhost:27017/handbook';
 var onConnect = function (err, db) {
-    app.use(function(request, response, next){
+    app.use(function (request, response, next) {
         request.db = db;
         next();
     });
-}
+};
 mongoClient.connect(mongoURI, onConnect);
 
 app.use('/', webRoutes);
@@ -39,10 +39,10 @@ app.use('/api', apiRoutes);
 app.use('/input', inputRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -50,23 +50,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/input.js
+++ routes/input.js
@@ -4,108 +4,117 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (request, response) {
-	response.render('input', {message: null, results: null});
+    response.render('input', {message: null, results: null});
 });
 
 router.post('/new', function (request, response) {
-	var collection = request.db.collection('articles');
-	var article = {
-		main_category: request.body.m_category,
-		sub_category: request.body.s_category,
-		sub_sub_category: request.body.ss_category,
-		topic: request.body.topic,
-		heading: request.body.heading,
-		content: request.body.contentText,
-		tags: request.body.tag,
-		timestamp: new Date()
-	};
-	var onInsert = function (err) {
-		if (err) {
-			console.log('error: ' + err);
-		}
-		else {
-			response.render('input', {message: 'Inserted', results: null})
-		}
-	}
-	collection.insertOne(article, onInsert);
+    var collection = request.db.collection('articles');
+    var article = {
+        main_category: request.body.m_category,
+        sub_category: request.body.s_category,
+        sub_sub_category: request.body.ss_category,
+        topic: request.body.topic,
+        heading: request.body.heading,
+        content: request.body.contentText,
+        tags: request.body.tag,
+        timestamp: new Date()
+    };
+    var onInsert = function (err) {
+        if (err) {
+            console.log('error: ' + err);
+        }
+        else {
+            response.render('input', {message: 'Inserted', results: null})
+        }
+    }
+    collection.insertOne(article, onInsert);
 });
 
 router.post('/find', function (request, response) {
-	var collection = request.db.collection('articles');
-	var topic = request.body.topic;
-	var onSearch = function (err, docs) {
-		if (err) {
-			console.log('error: ' + err);
-		}
-		else if (docs.length === 0) {
-			response.render('input', {message: 'Not Found', results: null});
-		}
-		else {
-			response.render('input', {results: docs, message: 'Search Successful'});
-		}
-	};
-	collection.find({topic: topic}, onSearch);
+    var collection = request.db.collection('articles');
+    var topic = request.body.topic;
+    var onSearch = function (err, docs) {
+        if (err) {
+            console.log('error: ' + err);
+        }
+        else if (docs.length === 0) {
+            response.render('input', {message: 'Not Found', results: null});
+        }
+        else {
+            response.render('input', {results: docs, message: 'Search Successful'});
+        }
+    };
+    collection.find({topic: topic}, onSearch);
 });
 
 router.post('/edit', function (request, response) {
-	var collection = request.db.collection('articles');
-	var updateCollection = request.db.collection('updates');
-	var newArticle = {
-		main_category: request.body.m_category,
-		sub_category: request.body.s_category,
-		sub_sub_category: request.body.ss_category,
-		topic: request.body.topic,
-		heading: request.body.heading,
-		content: request.body.contentText,
-		tags: request.body.tag
-	};
-	var onUpdate = function (err) {
-		if (err) {
-			console.log('error: ' + err);
-		}
-		else{
-			response.render('input', {message: 'Updated', results: null});
-			var updateInfo = {
-				topic: topic,
-				timestamp: new Date()
-			};
-			var onFinish = function (err) {
-				if (err) {
-					console.log('error: ' + err);
-				}
-			}
-			updateCollection.findAndModify({topic: updateInfo.topic},
-				{$set: {timestamp: updateInfo.timestamp}},
-				{
-	              safe: true,
-	              new: true,
-	              upsert: true
-			}, onFinish);
-		}
-	};
-	collection.findAndModify({topic: newArticle.topic},
-		{$set: {main_category: newArticle.main_category, sub_category: newArticle.sub_category, sub_sub_category: newArticle.sub_sub_category, heading: newArticle.heading, content: newArticle.content, tags: newArticle.tags}},
-			{
-              safe: true,
-              new: true,
-              upsert: true
-            }, onUpdate
-	);
+    var collection = request.db.collection('articles');
+    var updateCollection = request.db.collection('updates');
+    var newArticle = {
+        main_category: request.body.m_category,
+        sub_category: request.body.s_category,
+        sub_sub_category: request.body.ss_category,
+        topic: request.body.topic,
+        heading: request.body.heading,
+        content: request.body.contentText,
+        tags: request.body.tag
+    };
+    var onUpdate = function (err) {
+        if (err) {
+            console.log('error: ' + err);
+        }
+        else {
+            response.render('input', {message: 'Updated', results: null});
+            var updateInfo = {
+                topic: topic,
+                timestamp: new Date()
+            };
+            var onFinish = function (err) {
+                if (err) {
+                    console.log('error: ' + err);
+                }
+            }
+            updateCollection.findAndModify({topic: updateInfo.topic},
+                {$set: {timestamp: updateInfo.timestamp}},
+                {
+                    safe: true,
+                    new: true,
+                    upsert: true
+                }, onFinish);
+        }
+    };
+    collection.findAndModify({topic: newArticle.topic},
+        {
+            $set: {
+                main_category: newArticle.main_category,
+                sub_category: newArticle.sub_category,
+                sub_sub_category: newArticle.sub_sub_category,
+                heading: newArticle.heading,
+                content: newArticle.content,
+                tags: newArticle.tags
+            }
+        },
+        {
+            safe: true,
+            new: true,
+            upsert: true
+        }, onUpdate
+    );
 });
 
 router.post('/delete', function (request, response) {
-	var collection = request.db.collection('articles');
-	var topic = request.body.topic;
-	var onDelete = function (err, results) {
-		if (err) {
-			console.log('error: ' + err);
-		}
-		else {
-			console.log(results);
-			response.render('input', {message: 'Deleted', results: null});
-		}
-	};
-	collection.remove({topic: topic}, onDelete);
+    var collection = request.db.collection('articles');
+    var topic = request.body.topic;
+    var onDelete = function (err, results) {
+        if (err) {
+            console.log('error: ' + err);
+        }
+        else {
+            console.log(results);
+            response.render('input', {message: 'Deleted', results: null});
+        }
+    };
+    collection.remove({topic: topic}, onDelete);
 });
 
 module.exports = router;

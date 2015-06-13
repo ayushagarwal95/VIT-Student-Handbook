d214dfcbb9b4dbfd1c65ddeca1b385a98565da42
ayushagarwal95 <agarwalayush161@gmail.com>
2015-06-13 23:25:08 +0530

    Web Routes in Progress

--- routes/web.js
+++ routes/web.js
@@ -3,8 +3,23 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index');
+router.get('/', function (request, response) {
+    response.render('index');
 });
 
+router.post('/login', function (request, response) {
+    var regno = request.body.reg_num;
+    // TODO - Add suggestions functionality
+    response.render('main');
+});
+
+router.get('/main', function (request, response) {
+    response.render('main');
+});
+
+router.post('/search', function (request, response) {
+    var searchText = request.body.searchItem;
+    //TODO - mongo search on tags
+    
+});
 module.exports = router;

5018526b79f3c45d2163eb4746155b3c26c8656e
ayushagarwal95 <agarwalayush161@gmail.com>
2015-06-28 15:47:47 +0530

    Fixed API Route

--- routes/api.js
+++ routes/api.js
@@ -11,14 +11,20 @@ router.post('/updates', function (request, response) {
             response.status(500).send('Internal Server Error');
         }
         else {
+            console.log(docs);
             response.json(docs);
         }
     };
     var date = request.body.timestamp;
     var parsedDate = new Date(date).toISOString();
-    collection.find({
-        timestamp: {'$gte': {'$date': parsedDate}}
-    }, onSearch);
+    console.log(parsedDate);
+    var query = {};
+    query['timestamp'] = {
+            '$gte': {
+                '$date': parsedDate
+                }
+    };
+    collection.find(query).toArray(onSearch);
 });
 
 module.exports = router;

40ed615e7e8b9276a46f8653dbf6249c8d359029
BurnUalive <burnualive@hotmail.com>
2015-07-29 10:47:29 +0530

    checking headers

--- app.js
+++ app.js
@@ -39,7 +39,21 @@ var db = mongojs(mongoURI);
 
 app.use(function (request, response, next) {
     request.db = db;
-    next();
+
+        // Website you wish to allow to connect
+    response.header('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+    response.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+      //  res.header('Access-Control-Allow-Credentials', false);
+
+        next();
 });
 
 app.use('/', webRoutes);
--- routes/web.js
+++ routes/web.js
@@ -59,18 +59,6 @@ router.get('/search', function (request, response) {
 });
 
 router.get('/articles', function (request, response) {
-    // Website you wish to allow to connect
-    response.header('Access-Control-Allow-Origin', 'http://localhost:63342');
-
-    // Request methods you wish to allow
-    response.header('Access-Control-Allow-Methods', 'GET');
-
-    // Request headers you wish to allow
-    response.header('Access-Control-Allow-Headers', 'X-Requested-With');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    response.header('Access-Control-Allow-Credentials', false);
     var collection = request.db.collection('articles');
     var m_category = request.query.main_category;
     var s_category = request.query.sub_category;
@@ -80,7 +68,6 @@ router.get('/articles', function (request, response) {
                 response.status(500).send('Internal Server Error');
             }
             else {
-                log.log(docs.length);
                 response.json(docs);
             }
         });
